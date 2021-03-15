3c17a45f6c0ef6a400bec2ccefe86f435cd7d3b8
Y <you@example.com>
2021-03-15 19:24:21 +0100

    Puta python, amb lo bonics que és el tipat estàtic

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 from multiprocessing import Process, Lock
+from xmlrpc.server import SimpleXMLRPCServer
+from time import sleep
+import logging
+import os
 import redis
 
 WORKERS = {}
@@ -6,16 +10,23 @@ FREE_WORKERS = {}
 WORKER_ID = 0
 r = redis.Redis()
 
-def start_worker(id, lock)
+logging.basicConfig(level=logging.INFO)
+
+server = SimpleXMLRPCServer(
+    ('localhost', 9000),
+    logRequests=True,
+    allow_none=True)
+
+def start_worker(id, lock):
     global r
-    while(True)
-        tasca = r.blpop()
+    while(True):
+        tasca = r.blpop('cua')
         lock.acquire()
-        # fer tasca
+        print(tasca)
         lock.release()
 
 
-def create_worker()
+def create_worker():
     global WORKERS
     global WORKER_ID
     global FREE_WORKERS
@@ -25,19 +36,33 @@ def create_worker()
     proc.start()
     WORKERS[WORKER_ID] = proc
     FREE_WORKERS[WORKER_ID] = lock
-
     WORKER_ID+=1
+    print(proc)
+    return WORKER_ID-1
 
-def eliminate_worker(id)
+    
+
+def eliminate_worker(id):
     global WORKERS
     global WORKER_ID
     global FREE_WORKERS
 
     FREE_WORKERS[id].acquire()
     WORKERS[WORKER_ID].join()
-    FREE_WORKERS[id].release()
+    WORKERS.remove(WORKERS[id])
+    FREE_WORKERS.remove(FREE_WORKERS[id])
 
-def put_task(task)
+def put_task(task):
     global r
-    r.rpush(cua, task)
+    r.rpush('cua', task)
  
+server.register_function(put_task)
+server.register_function(eliminate_worker)
+server.register_function(create_worker)
+create_worker()
+try:
+    print('Use Control-C to exit')
+    server.serve_forever()
+except KeyboardInterrupt:
+    print('Exiting')
+
--- webpage/main.py
+++ webpage/main.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
+import wtforms
+import xmlrpc.client
+
+proxy = xmlrpc.client.ServerProxy('http://localhost:9000')
 
 app = Flask(__name__)
 
@@ -12,28 +16,46 @@ app.config['SECRET_KEY'] = 'C2HWGVoMGfNTBsrYQg8EcMrdTimkZfAb'
 # Flask-Bootstrap requires this line
 Bootstrap(app)
 
+class OptionalIfFieldEqualTo(wtforms.validators.Optional):
+    # a validator which makes a field optional if
+    # another field has a desired value
+
+    def __init__(self, other_field_name, value, *args, **kwargs):
+        self.other_field_name = other_field_name
+        self.value = value
+        super(OptionalIfFieldEqualTo, self).__init__(*args, **kwargs)
+
+    def __call__(self, form, field):
+        other_field = form._fields.get(self.other_field_name)
+        if other_field is None:
+            raise Exception('no field named "%s" in form' % self.other_field_name)
+        if other_field.data == self.value:
+            super(OptionalIfFieldEqualTo, self).__call__(form, field)
+
 class NameForm(FlaskForm):
-    name = StringField('Which actor is your favorite?', validators=[DataRequired()])
+    function = SelectField('Function', choices=[('Put Task'), ('Create Worker'), ('Eliminate Worker')],validators=[DataRequired()])
+    task = SelectField('Task', choices=[('Patata'), ('Poma')], validators=[OptionalIfFieldEqualTo('function', 'Put Task')])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    names = {"783dhfuisdhfuihdufasdhfsd", "asdfa"}
     # you must tell the variable 'form' what you named the class, above
     # 'form' is the variable name used in this template: index.html
     form = NameForm()
     message = ""
     if form.validate_on_submit():
-        name = form.name.data
-        if name == "a":
-            # empty the form field
-            form.name.data = ""
-            # id = get_id(ACTORS, name)
-            # redirect the browser to another route and template
-            return redirect("https://campusvirtual.urv.cat")
-        else:
-            message = "That actor is not in our database."
-    return render_template('index.html', names=names, form=form, message=message)
+        function = form.function.data
+        task = form.task.data
+        if function == 'Put Task':
+            proxy.put_task(task)
+            message = 'Tasca ' + str(task) + ' afegida' 
+        if function == 'Create Worker':
+            id = proxy.create_worker()
+            message = 'Node ' + str(id) + ' creat'
+        if function == 'Eliminate Worker':
+            proxy.eliminate_worker(0)
+            message = 'Node eliminat'
+    return render_template('index.html', form=form, message=message)
 
 if __name__ == "__main__":
     app.run()
